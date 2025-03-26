7ea648903be90209571432aff61a43731cf20890
GiovannyPadilla <146996637+GiovannyPadilla@users.noreply.github.com>
2025-03-26 07:26:39 -0500

    Agregar archivos de proyecto.

---
+++ Primer proyecto clase 01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Primer_proyecto_clase_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
